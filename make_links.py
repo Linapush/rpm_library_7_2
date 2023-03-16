1539d586ae495df8e7c6b69dbb005bd185f98fba
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-16 16:19:33 +0300

    make links

--- make_links.py
+++ make_links.py
@@ -1,6 +1,7 @@
 from psycopg2 import connect
 from dotenv import load_dotenv
 from os import getenv
+from random import choice
 
 load_dotenv()
 credentials = {
@@ -21,13 +22,13 @@ book_ids = [id[0] for id in cursor.fetchall()]
 cursor.execute(f'{get_ids} library.author')
 author_ids = [id[0] for id in cursor.fetchall()]
 
-books_authors_ratio = len(book_ids) // len(author_ids)
 request = "INSERT INTO library.book_author (book_id, author_id) VALUES ('{0}', '{1}')"
-
-for book_ind in range(len(book_ids)//2):
-    author_ind = book_ind // books_authors_ratio
-    cursor.execute(request.format(book_ids[book_ind], author_ids[author_ind]))
-    print('Current index: ', book_ind)
+i = 0
+for book_id in book_ids:
+    author_id = choice(author_ids)
+    cursor.execute(request.format(book_id, author_id))
+    print(f'Current progress: {i / len(book_ids)}%')
+    i += 1
 
 connection.commit()
 cursor.close()
