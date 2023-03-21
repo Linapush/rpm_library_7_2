f5d5671d14956e84279e27348b142f215813547f
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-21 11:49:49 +0300

    admin panel models

--- library/library_app/admin.py
+++ library/library_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book, Genre, Author
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    model = Author
