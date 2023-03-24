6b591c172538867b4af4921c15c1ba7c7d463a94
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-24 15:07:38 +0300

    models, filters

--- library/library_app/admin.py
+++ library/library_app/admin.py
@@ -1,14 +1,51 @@
 from django.contrib import admin
-from .models import Book, Genre, Author
+from .models import Book, Genre, Author, BookAuthor, BookGenre
+from datetime import datetime
+
+
+class BookAuthor_inline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1
+
+class BookGenre_inline(admin.TabularInline):
+    model = BookGenre
+    extra = 1
+
+class RecencyBookFilter(admin.SimpleListFilter):
+    title='recency'
+    parameter_name='recency'
+
+    def lookups(self, *_):
+        return (
+            ('10yo', 'Written in the last 10 years'),
+            ('20yo', 'Written in the last 20 years'),
+        )
+    
+    def queryset(self, _, queryset):
+        def filter(queryset, year):
+            return queryset.filter(year__gte = year)
+        if self.value() == '10yo':
+            return filter(queryset, datetime.now().year - 10)
+        elif self.value() == '20yo':
+            return filter(queryset, datetime.now().year - 20)
+        return queryset
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     model = Book
-
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
-    model = Genre
+    inlines = (BookAuthor_inline, BookGenre_inline)
+    list_filter = (
+        'genres',
+        'type',
+        RecencyBookFilter,
+        'authors'
+    )
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     model = Author
+    inlines = (BookAuthor_inline,)
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
--- library/library_app/models.py
+++ library/library_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from uuid import uuid4
+from django.core.exceptions import ValidationError
+from datetime import datetime
+
 
 class UUIDMixin(models.Model):
-    id = models.UUIDField(primary_key=True, defaul=uuid4)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
 
     class Meta:
         abstract = True
@@ -20,19 +23,47 @@ class ModifiedMixin(models.Model):
         abstract = True
 
 class Author(UUIDMixin, CreatedMixin, ModifiedMixin):
-    full_name = models.TextField()
+    full_name = models.CharField(max_length=40)
+    books = models.ManyToManyField('Book', through='BookAuthor')
+
+    def __str__(self):
+        return self.full_name
 
     class Meta:
         db_table = 'author'
 
+def positive_int(num: int):
+    if num <= 0:
+        raise ValidationError(
+            f'Value {num} is less or equal zero',
+            params={'value': num}
+        )
+
+def year_validator(year: int):
+    current_year = datetime.now().year
+    if year > current_year:
+        raise ValidationError(
+            f'The year field must be less or equal {current_year}',
+            params={'year': year}
+        )
+
+book_types = (
+    ('book', 'book'),
+    ('journal', 'journal')
+)
 
 class Book(UUIDMixin, CreatedMixin, ModifiedMixin):
-    title = models.TextField()
+    title = models.CharField(max_length=40)
     description = models.TextField(blank=True, null=True)
-    volume = models.IntegerField(blank=True, null=True)
-    age_limit = models.IntegerField(blank=True, null=True)
-    year = models.IntegerField(blank=True, null=True)
-    type = models.TextField()
+    volume = models.IntegerField(blank=True, null=True, validators=[positive_int])
+    age_limit = models.IntegerField(blank=True, null=True, validators=[positive_int])
+    year = models.IntegerField(blank=True, null=True, validators=[year_validator])
+    type = models.CharField(max_length=20, choices=book_types, blank=False, null=False)
+    authors = models.ManyToManyField(Author, through='BookAuthor')
+    genres = models.ManyToManyField('Genre', through='BookGenre')
+
+    def __str__(self):
+        return f'{self.title}, {self.type}, {self.year}'
 
     class Meta:
         db_table = 'book'
@@ -46,9 +77,13 @@ class BookAuthor(UUIDMixin, CreatedMixin):
         db_table = 'book_author'
         unique_together = (('book', 'author'),)
 
+
 class Genre(UUIDMixin, CreatedMixin, ModifiedMixin):
-    name = models.TextField()
+    name = models.CharField(max_length=30)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         db_table = 'genre'
