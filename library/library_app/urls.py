8a8d9ee999d5d912903e6b5d73a1f18fd5d7af74
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-24 16:00:20 +0300

    views with catalogs

--- library/library_app/urls.py
+++ library/library_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from .views import custom_main
+from .views import BookListView, AuthorListView, GenreListView
 
 
 urlpatterns = [
     path('homepage/', custom_main, name='homepage'),
+    path('books/', BookListView.as_view(), name='books'),
+    path('authors/', AuthorListView.as_view(), name='authors'),
+    path('genres', GenreListView.as_view(), name='genres'),
 ]
--- library/library_app/views.py
+++ library/library_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .models import Book, Genre, Author
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+
+
+PAGINATOR_THRESHOLD = 20
 
 TEMPLATE_MAIN = 'index.html'
 
@@ -13,3 +18,30 @@ def custom_main(request):
             'authors': Author.objects.all().count(),
         }
     )
+
+def catalog_view(cls_model, context_name, template):
+    class CustomListView(ListView):
+        model = cls_model
+        template_name = template
+        paginate_by = PAGINATOR_THRESHOLD
+        context_object_name = context_name
+
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            objects = cls_model.objects.all()
+            paginator = Paginator(objects, PAGINATOR_THRESHOLD)
+            page = self.request.GET.get('page')
+            page_obj = paginator.get_page(page)
+            context[f'{context_name}_list'] = page_obj
+            return context
+
+    return CustomListView
+
+CATALOG = 'catalog'
+BOOKS_CATALOG = f'{CATALOG}/books.html'
+AUTHORS_CATALOG = f'{CATALOG}/authors.html'
+GENRES_CATALOG = f'{CATALOG}/genres.html'
+
+BookListView = catalog_view(Book, 'books', BOOKS_CATALOG)
+AuthorListView = catalog_view(Author, 'authors', AUTHORS_CATALOG)
+GenreListView = catalog_view(Genre, 'genres', GENRES_CATALOG)
