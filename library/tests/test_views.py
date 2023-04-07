4a7e22961313e05a947718bd842af2eb258c49e9
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-04-07 11:58:24 +0300

    tests on all views

--- library/tests/test_views.py
+++ library/tests/test_views.py
@@ -6,15 +6,16 @@ from django.contrib.auth.models import User
 from django.test.client import Client
 
 
-def test_listview(cls_model, url, page_name, template, attrs):
-    class ListViewTest(TestCase):
+def test_view(url, page_name, template, cls_model=None, attrs=None):
+    class ViewTest(TestCase):
         def setUp(self):
             self.client = Client()
             username = password = 'test'
             self.user = User.objects.create_user(username=username, email='a@a.com', password=password)
             self.client.login(username=username, password=password)
-            # for _ in range(100):
-            #     cls_model.objects.create(**attrs)
+            if cls_model:
+                for _ in range(100):
+                    cls_model.objects.create(**attrs)
         
         def test_exists_by_url(self):
             self.assertEqual(self.client.get(url).status_code, OK)
@@ -27,11 +28,13 @@ def test_listview(cls_model, url, page_name, template, attrs):
             self.assertEqual(response.status_code, OK)
             self.assertTemplateUsed(response, template)
 
-    return ListViewTest
+    return ViewTest
 
 genre_attrs = {'name': 'genre'}
-GenreListViewTest = test_listview(Genre, '/genres/', 'genres', 'catalog/genres.html', genre_attrs)
+GenreListViewTest = test_view('/genres/', 'genres', 'catalog/genres.html', Genre, genre_attrs)
 author_attrs = {'full_name': 'Fool Name'}
-AuthorListViewTest = test_listview(Author, '/authors/', 'authors', 'catalog/authors.html', author_attrs)
+AuthorListViewTest = test_view('/authors/', 'authors', 'catalog/authors.html', Author, author_attrs)
 book_attrs = {'title': 'Title'}
-BookListViewTest = test_listview(Book, '/books/', 'books', 'catalog/books.html', book_attrs)
+BookListViewTest = test_view('/books/', 'books', 'catalog/books.html', Book, book_attrs)
+HomePageTest = test_view('', 'homepage', 'index.html')
+WeatherPageTest = test_view('/weather/', 'weather', 'pages/weather.html')
